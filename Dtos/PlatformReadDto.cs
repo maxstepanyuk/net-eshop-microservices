2fbf612a7ed74b449d33fe07d43095372f69260b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2022-11-05 17:56:00 +0200

    Data Layer - Data Transfer Objects

---
+++ Dtos/PlatformReadDto.cs
@@ -0,0 +1,10 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformServiceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pubihser { get; set; }
+        public string Cost { get; set; }
+    }
+}
---
+++ Profiles/PlatformProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using PlatformService.Models;
+using PlatformService.Dtos;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile(){
+            //Source -> Target
+            CreateMap<Platform, PlatformCreateDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
