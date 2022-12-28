8b17cd858012e67e6d48e9abed68affd7df3a192
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2022-12-28 15:25:52 +0200

    added dotnet command and todo files

---
+++ dotnet command notes.sh
@@ -0,0 +1,13 @@
+# new webapi
+dotnet new webapi -n CustomerService --framework "net6.0" 
+
+# packages
+dotnet add package AutoMapper.Extensions.Microsoft.DependencyInjection
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.InMemory
+
+# navigation
+cd ..
+clear
+ls
