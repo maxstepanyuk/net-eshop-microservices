774d9d683af68566cba5eabc07bbe9b788a7b0e4
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2023-01-08 15:44:28 +0200

    product service - Added Back the Startup Class like in 5

---
+++ ProductService/Startup.cs
@@ -0,0 +1,38 @@
+namespace ProductService
+{
+    public static class Startup
+    {
+        public static WebApplication InitializeApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+            var app = builder.Build();
+            Configure(app);
+            return app;
+        }
+        
+        public static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+
+        public static void Configure(WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
